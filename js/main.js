008db5f65ac6b9d924fe511847817c34b89875e4
DESKTOP-H8LEKO9\Alexandr <alphapes01@gmail.com>
2022-10-04 00:05:03 +0700

    fix bugs, add adaptive, add animate

--- js/main.js
+++ js/main.js
@@ -5,31 +5,95 @@ const activeLang = document.querySelectorAll('.custom-select__active span');
 const titleOne = document.querySelector('.animate-one');
 const animateTwo = document.querySelector('.users-flex');
 const animateThree = document.querySelector('.community');
+const link = document.querySelector('.link');
 const animateFour = document.querySelector('.animation-four');
 const sectionTwo = document.querySelector('.section-2');
+const animate2 = document.querySelector('.animation-two')
+const animate3 = document.querySelector('.animation-three')
+const animate4 = document.querySelector('.animation-four');
+const animate5 = document.querySelector('.section_centre');
+const animate6 = document.querySelectorAll('.animate-six');
+const animate7 = document.querySelector('.animate-seven');
+const animate8 = document.querySelector('.animate-eight');
+const animate9 = document.querySelectorAll('.animate-nine');
+const arrows = document.querySelectorAll('.desc');
+const animate10 = document.querySelector('.animate-ten');
+const animate11 = document.querySelectorAll('.animate-eleven');
+const animate12 = document.querySelector('.animate-twelve');
+const animate13 = document.querySelectorAll('.animate-thirteen');
+const animate14 = document.querySelector('.animate-fourteen');
+const animate15 = document.querySelector('.animate-fifteen');
 
 const window_height = (window.innerHeight ? window.innerHeight : (document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.offsetHeight));
+const sections = document.querySelectorAll(".animate_block");
 
-// window.addEventListener('scroll', function() {
- 
-//     const top = document.body.scrollTop;
-//     const find = document.querySelector('.section-2').offsetTop;
-//         // if(el_top > top && ((el.clientHeight + el_top) < (top + window_height))) {
-//         //     el.style.backgroundColor = "#999";
-//         // }
-//         console.log(find);
-//         console.log(top);
-//         if (sectionTwo.offsetTop <= top) {
-//           console.log('3');
-//         }
+const cb = (entries) => {
+  entries.forEach((entry) => {
+    // entry.target.classList.remove("animate", entry.isIntersecting);
+    if(entry.isIntersecting) {
+      if(entry.target.id === '#zaim') {
+        setTimeout(() => {
+          animate2.classList.remove('hidden');
+        }, 200)
+        setTimeout(() => {
+          animate3.classList.remove('hidden');
+        }, 500)
+        setTimeout(() => {
+          arrows.forEach(i => i.classList.remove('hidden'));
+        }, 800)
+      }
+      if(entry.target.id === '#karma') {
+        setTimeout(() => {
+          animate4.classList.remove('hidden');
+        }, 100)
+        setTimeout(() => {
+          animate5.classList.remove('hidden');
+        }, 500)
+        setTimeout(() => {
+          animate6.forEach(i => i.classList.remove('hidden'))
+        }, 1100)
+      }
+      if(entry.target.id === '#limit') {
+        setTimeout(() => {
+          animate7.classList.remove('hidden');
+        }, 100)
+        setTimeout(() => {
+          animate8.classList.remove('hidden');
+        }, 300);
+        setTimeout(() => {
+          animate9.forEach(i => i.classList.remove('hidden'));
+          animate11.forEach(i => i.classList.remove('hidden'));
+          animate10.classList.remove('hidden')
+          animate12.classList.remove('hidden');
+          animate13.forEach(i => i.classList.remove('hidden'));
+        }, 800);
+      }
+      if(entry.target.id === 'five') {
+        setTimeout(() => {
+          animate14.classList.remove('hidden');
+          animate15.classList.remove('hidden');
+        }, 200)
+      }
 
-//       })
+    }
+  });
+};
+
+const sectionObserver = new IntersectionObserver(cb, {
+  threshold: [0.2, 0.5, 0.8]
+});
+
+sections.forEach((sec) => {
+  sectionObserver.observe(sec);
+});
 
 titleOne.classList.remove('hidden');
+animateTwo.classList.remove('hidden');
 setTimeout(() => {
-  animateTwo.classList.remove('hidden');
+
   animateThree.classList.remove('hidden');
-}, 400)
+  link.classList.remove('hidden');
+}, 500)
 
 if (sectionTwo.scrollIntoView) {
   console.log('2')
@@ -67,3 +131,4 @@ for (anchor of anchors) {
     })
   }
 }
+
